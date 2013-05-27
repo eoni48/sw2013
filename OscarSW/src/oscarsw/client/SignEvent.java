4ffe04dec4bd1c4c5856e18c7f7270085ad2efe0
ocrespo <ocresposalazar@gmail.com>
2013-05-27 12:41:59 +0200

    Añadido control de seguridad en outEvent

--- OscarSW/src/oscarsw/client/SignEvent.java
+++ OscarSW/src/oscarsw/client/SignEvent.java
@@ -12,6 +12,7 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 import oscarsw.dao.DAO;
+import oscarsw.data.Competitor;
 
 /**
  * @author roni
@@ -22,10 +23,25 @@ public class SignEvent extends HttpServlet{
 	protected void doPost(HttpServletRequest request,HttpServletResponse response)throws ServletException,IOException{
 		HttpSession session = request.getSession();
 		String nick = (String) session.getAttribute("nick");
+		String level = (String) session.getAttribute("type");
 		String id = request.getParameter("id");
+		
+		
+		
 		if(nick == null){
 			response.sendRedirect("/log.jsp");
 			return;
+		}
+		else if(level.equals("organizer")){
+			response.sendRedirect("/lista_eventos.jsp");
+			return;
+		}
+		else{
+			Competitor user = (DAO.getInstance()).getCompetitor(nick);
+			if(user == null || !user.isSign(Long.valueOf(id))){
+				response.sendRedirect("/lista_eventos.jsp");
+				return;
+			}
 		}
 		(DAO.getInstance()).addSignEvent(Long.valueOf(id), nick);
 		response.sendRedirect("/lista_eventos.jsp");
